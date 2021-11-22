9a5238105eafc74373f496d26f97cf095daa4696
Péter Pandula <pandulapeter@gmail.com>
2021-11-22 15:16:26 +0200

    Update third party dependencies.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -6,19 +6,19 @@ object Versions {
 
     // Project
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 30
-    const val KOTLIN = "1.5.20"
+    const val TARGET_SDK_VERSION = 31
+    const val KOTLIN = "1.5.30"
     const val JAVA = 1.8
 
     // Libraries
-    const val APP_COMPAT = "1.3.1"
-    const val BEAGLE = "2.6.2"
+    const val APP_COMPAT = "1.4.0"
+    const val BEAGLE = "2.6.5"
     const val COIL = "1.3.1"
-    const val CONSTRAINT_LAYOUT = "2.1.0"
-    const val COROUTINES = "1.5.1"
-    const val GRADLE = "7.0.0"
-    const val KOIN = "3.1.2"
-    const val LIFECYCLE = "2.3.1"
+    const val CONSTRAINT_LAYOUT = "2.1.2"
+    const val COROUTINES = "1.5.2"
+    const val GRADLE = "7.0.3"
+    const val KOIN = "3.1.3"
+    const val LIFECYCLE = "2.4.0"
     const val MATERIAL_COMPONENTS = "1.4.0"
     const val MOSHI = "1.12.0"
     const val OK_HTTP = "4.9.1"
